5ee116b6aecac250822c2ff5e51c6098379f8b63
juryalbuzaid-png <juryalbuzaid@gmail.com>
2026-04-22 20:57:35 +0300

    login script

---
+++ script.js
@@ -0,0 +1,16 @@
+document.addEventListener('DOMContentLoaded', function() {
+
+    const loginForm = document.getElementById('loginForm');
+    if (loginForm) {
+        loginForm.addEventListener('submit', function(e) {
+            e.preventDefault();
+            const user = document.getElementById('username').value;
+            const pass = document.getElementById('password').value;
+            
+            if (user === "Dr.Nabeel" && pass === "BestWebDr@IAU") {
+                window.location.href = "dashboard.html";
+            } else {
+                alert("Incorrect username or password. Please try again.");
+            }
+        });
+    }
